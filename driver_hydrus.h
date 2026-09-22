835502d52193181f1f5861408575deb164a0d8ca
kplysa <94488397+kplysa@users.noreply.github.com>
2026-09-22 12:45:57 +0200

    Add Hydrus 0C14 volume and 0B3B flow
    Updated get_values method to retrieve total water volume from get_0C14 and added current volume flow from get_0B3B.

--- driver_hydrus.h
+++ driver_hydrus.h
@@ -13,10 +13,17 @@
 struct Hydrus: Driver
 {
   Hydrus(std::string key = "") : Driver(std::string("hydrus"), key) {};
-  virtual esphome::optional<std::map<std::string, double>> get_values(std::vector<unsigned char> &telegram) override {
+
+  virtual esphome::optional<std::map<std::string, double>> get_values(
+      std::vector<unsigned char> &telegram) override {
+
     std::map<std::string, double> ret_val{};
 
-    add_to_map(ret_val, "total_water_m3", this->get_0C13(telegram));
+    // Total water volume from 0C14, returned in m3.
+    add_to_map(ret_val, "total_water_m3", this->get_0C14(telegram));
+
+    // Current volume flow from 0B3B, returned in l/h.
+    add_to_map(ret_val, "volume_flow_lh", this->get_0B3B(telegram));
 
     if (ret_val.size() > 0) {
       return ret_val;
